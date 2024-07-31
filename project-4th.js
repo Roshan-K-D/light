a462ff5a6bd76d4544104b7212490453b1ee7f46
Roshan-K-D <roshandubey374@gmail.com>
2024-07-31 11:28:59 +0530

    light a book social media

---
+++ project-4th.js
@@ -0,0 +1,110 @@
+
+// animation with gsap
+
+// nev-bar
+let tl =gsap.timeline();
+
+tl.from(".nev-bar .logo",{
+    opacity:0,
+    y:-100,
+    duraction:1,
+    delay:.5,
+})
+
+tl.from(".option",{
+    opacity:0,
+    y:-10,
+    duraction:.2,
+})
+
+tl.from(".option a",{
+    opacity:0,
+    y:-20,
+    duraction:1,
+    stagger:.3,
+})
+
+tl.from(".login",{
+    opacity:0,
+    y:-10,
+    duraction:.2,
+})
+
+tl.from(".nev-bar .signup",{
+    opacity:0,
+    y:-10,
+    duraction:.3,
+})
+
+// hero
+
+tl.from(".hero .hero-img",{
+    opacity:0,
+    x:50,
+    duraction:.6,
+})
+
+tl.from(".hero h1",{
+    opacity:0,
+    x:50,
+    duraction:.4,
+})
+
+tl.from(".hero p",{
+    opacity:0,
+    x:50,
+    duraction:.4,
+})
+
+
+tl.from(".hero button",{
+    opacity:0,
+    x:50,
+    duraction:.4,
+})
+
+// page1
+
+tl.to(".upper-page1",{
+    transform:"translateX(-190%)",
+    scrollTrigger:{
+        trigger:".upper-page1 ",
+        scroller:"body",
+        // markers:true,
+        start:"top 0%",
+        end:"top -150%",
+        scrub:1,
+        pin:true
+    }
+})
+
+var ti = gsap.timeline()
+
+gsap.from(".role-page h2",{
+    opacity:0,
+    y:-20,
+    duraction:1,
+    scrollTrigger:{
+        trigger:".role-page",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 50%",
+    }
+})
+
+ti.from(".roles div",{
+    opacity:0,
+    y:-50,
+    duraction:1,
+    stagger:.3,
+    scrollTrigger:{
+        trigger:".role-page",
+        scoller:"body",
+        // markers:true,
+        start:"top 50%",
+        end:"top 20%",
+        scrub:2, 
+    }
+})
+
